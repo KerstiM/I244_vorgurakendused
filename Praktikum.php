52312076e78774deb46c9015af2783515cd1aa44
Kersti Miller <kmiller@klass2-12-l.intra.itcollege.ee>
2016-02-14 16:44:42 +0200

    Kolmas commit

---
+++ Praktikum.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Kersti esimene HTML leht</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="Stiilid.css">
+ 		<script src="timer.js"></script> 
+	</head>
+
+	<body>
+
+		<h1>Kersti esimene HTML leht</h1>
+		<p>Tere tere tere.</p>
+
+		<span id="timer"></span>
+		
+		<?php
+		include("counter.php");
+		?>
+
+		<p id="para1">Me too!</p>
+		<h2>catsod</h2>
+		<img src="https://cdn.mysmelly.com/image:/sitefs/perm/pi/b/q/r/c/501.280.250_f1.jpg?9251294847" alt="Catsod" style="width:304px;height:228px;">
+
+
+
+	</body>
+</html>
