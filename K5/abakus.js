fec0dd9d45af7b23c2c07e16eb5dbe144904a7fe
KerstiM <kerstim@gmail.com>
2016-03-20 22:03:51 +0200

    VIIES teema kolmas

--- K5/abakus.js
+++ K5/abakus.js
@@ -1,9 +1,13 @@
 	window.onload = function() {
+		//selekteerin bead classi elemendid
 		var bead = document.querySelectorAll(".bead");
+		//kasutan for tsüklit kõigi massiivi elementide itereerimiseks	
+
 			for (i = 0; i < bead.length; i++) { 
+				//mis ta selle tsükli jooksul teeb? kliki peale liigub kuhugi.
     			bead[i].onclick = liiguta;
 			}
-			
+			//funktsioon, mis läheb tööle, kui klikkida
  			function liiguta() {
  				if (this.style.cssFloat == "left") {
 					this.style.cssFloat = "right";
