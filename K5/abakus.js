31cf6656c52d468c68098b31ca7471e85ee18b39
KerstiM <kerstim@gmail.com>
2016-03-20 21:57:41 +0200

    viies teema

--- K5/abakus.js
+++ K5/abakus.js
@@ -1,21 +1,15 @@
-	
 	window.onload = function() {
-		
-		
+		var bead = document.querySelectorAll(".bead");
+			for (i = 0; i < bead.length; i++) { 
+    			bead[i].onclick = liiguta;
+			}
+			
+ 			function liiguta() {
+ 				if (this.style.cssFloat == "left") {
+					this.style.cssFloat = "right";
+ 				} else {
+					this.style.cssFloat = "left";		
+				}
+			}	
 		};
-		
-	
-hankida kõikide bead klassi elementide objektid, salvestada nad muutujasse (tekib massiiv)
 
-Kasutada for tsüklit üle kõigi massiivi elementide itereerimiseks
-
-iga elemendi puhul lugeda tema stiilist tema float stiilireegli väärtus. 
-NB! kuna float tähendab js-is ka komakohaga arvu, siis CSS omaduse jaoks on kasutusel 
-cssFloat omadus.
-
-Kontrollida eelnevas punktis saadud väärtust if lauses. 
-Kui väärtus on left, siis määrata elemendi uueks cssFloat väärtuseks 
-right ja vastupidi.
-Kui tahad kontrollida, et just õige element muudab asukohta, 
-siis anna HTML failis bead klassi elementidele mingi sisu nt. jrk. numbrid. 
-(et tulemus oleks ilusam, tasub bead klassile määrata teksti joondamine keskele)
