c847e076439d3fd0dfb65348df07e203a82ca695
KerstiM <kerstim@gmail.com>
2016-04-17 20:10:28 +0300

    KT9 esimene

--- K7/kassiArray.php
+++ K7/kassiArray.php
@@ -3,10 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Include tekst</title>
-    
-    <style>
-   
-    </style>
+  
 </head>
 <body>
 
---
+++ K7/kontroller.php
@@ -0,0 +1,16 @@
+<?php 
+	$pildid=array(
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Chrysanthemum.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Chrysanthemum.jpg", "alt"=>"Chrysanthemum"),
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Desert.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Desert.jpg", "alt"=>"Desert"),
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Hydrangeas.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Hydrangeas.jpg", "alt"=>"Hydrangeas"),	
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Jellyfish.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Jellyfish.jpg", "alt"=>"Jellyfish"),
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Koala.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Koala.jpg", "alt"=>"Koala"),
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Lighthouse.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Lighthouse.jpg", "alt"=>"Lighthouse"),	
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Penguins.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Penguins.jpg", "alt"=>"Penguins"),
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/Tulips.jpg", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/Tulips.jpg", "alt"=>"Tulips"),
+	  array("big"=>"big/http://enos.itcollege.ee/~kmiller/vorgurakendused/img/untitled-6.png", "small"=>"small/http://enos.itcollege.ee/~kmiller/vorgurakendused/thumb/untitled-6.png", "alt"=>"untitled-6"),	
+ 	);
+	  foreach($pildid as $inner){
+		include("galerii.html");
+}	
+?>
